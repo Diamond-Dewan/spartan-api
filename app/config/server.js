160b27094b773c3f35a9196c23492edcdeb036d8
Diamond <diamonddewan05@gmail.com>
2021-08-17 01:18:43 +0600

    remove project

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b3e2c7f0ad00d3589ada35a33cf5b42d'),
-    },
-  },
-});
--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7f5d475e-983a-4df4-99e1-9884eae4b5f9'
-};
