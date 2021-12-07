cd3f4ab5d2bef08c2d14047fad209fc0944b8349
Diamond <diamonddewan05@gmail.com>
2021-12-07 08:45:02 +0600

    skip if exits

--- backend/api/batch/services/batch.js
+++ backend/api/batch/services/batch.js
@@ -3,14 +3,33 @@
 const { fetchProducts } = require("./paapiService");
 const { findByName } = require("./utils");
 
+const create = async (modelName, properties) => {
+  console.log(`Batch::Service::Create`);
+  const item = await strapi.query(modelName).create({ ...properties });
+  return item;
+};
+
+const createBatch = async (keys) => {
+  const modelName = 'batch';
+  const keywords = keys && keys.toString();
+  const existingBatch = await strapi.query(modelName).find({ keywords });
+  if (existingBatch.length > 0) {
+    return existingBatch[0];
+  } else {
+    const batch = await create(modelName, {
+      keywords,
+    });
+    
+    return batch;
+  }
+};
+
 const getDepartmentId = async (departmentName) => {
   console.log(`Batch::Service::GetDepartmentId::Name: `, departmentName);
   const modelName = 'department';
   const item = await findByName(modelName, departmentName);
   if (!item) {
-    const department = await strapi.query(modelName).create({
-      name: departmentName
-    });
+    const department = await create(modelName, { name: departmentName });
 
     return department.id;
   }
@@ -25,13 +44,13 @@ const getCategoryId = async (departmentId, categoryName) => {
   const item = await findByName(modelName, categoryName);
   console.log(`Batch::Service::GetCategoryId::items`, item);
   if (!item) {
-    const category = await strapi.query(modelName).create({
+    const category = await create(modelName, {
       name: categoryName,
       department: [departmentId],
       created_by: 1,
       updated_by: 1,
     });
-    console.log(`Crated Category:: `, category);
+    console.log(`Created Category:: `, category);
 
     return category.id;
   } else {
@@ -39,39 +58,71 @@ const getCategoryId = async (departmentId, categoryName) => {
   }
 };
 
-const generateSlug = async (title='') => {
-  const slug = title.replaceAll(' ', '-');
+const generateSlug = (title='') => {
+  const slug = title.replace(/\s/g, "-");
+  console.log(`Generated Slug: `, slug);
 
   return slug;
 }
 
-const createPost = async (keyword, product) => {
+const createPost = async (keyword, batchNumber) => {
   console.log(`Batch::Service::CreatePost`);
+  const modelName = 'post'
   // get title
   const title = keyword;
-  // create slug
+  // create slugj
   const slug = generateSlug(title);
-  // get department 
-  const departmentName = product.ItemInfo && product.ItemInfo.Classifications.Binding && product.ItemInfo.Classifications.Binding.DisplayValue;
+  const existingPost = await strapi.query(modelName).find({ slug });
+  // return if exists
+  if (existingPost.length > 0) {
+    console.log(`Existing Posts: `, existingPost.length);
+    console.log(`Slug: `, existingPost[0].slug);
+    console.log(`Skipping....\n`);
+
+    return null;
+  };
+  
+  // fetch products
+  const products = await fetchProducts(keyword);
+  console.log(`Batch::Service:: ${products.length} items found for ${keyword}`);
+  
+  // body
+  const ranklist = products.map((product) => {
+    return {
+      title: product.ItemInfo && product.ItemInfo.Title && product.ItemInfo.Title.DisplayValue || undefined,
+      images: product.Images && product.Images.Primary || undefined,
+      contents: product.ItemInfo && product.ItemInfo.Features && product.ItemInfo.Features.DisplayValues || undefined,
+    }
+  });
+  // get department
+  const departmentName = products[0].ItemInfo && products[0].ItemInfo.Classifications.Binding && products[0].ItemInfo.Classifications.Binding.DisplayValue;
   const deptId = await getDepartmentId(departmentName);
   // get category
-  const categoryName = product.ItemInfo && product.ItemInfo.Classifications.ProductGroup && product.ItemInfo.Classifications.ProductGroup.DisplayValue;
+  const categoryName = products[0].ItemInfo && products[0].ItemInfo.Classifications.ProductGroup && products[0].ItemInfo.Classifications.ProductGroup.DisplayValue;
   const categoryId = await getCategoryId(deptId, categoryName);
   
-}
+  // create post
+  await strapi.query(modelName).create({
+    title,
+    slug,
+    rankedList: { ranklist },
+    batch: [batchNumber],
+    categories: [categoryId],
+  });
+};
 
 module.exports = {
   save: async (keywords = []) => {
-    // const length = keywords.length;
-    for (const keyword of keywords) {
-      const products = await fetchProducts(keyword);
-      console.log(`Batch::Service:: ${products.length} items found for ${keyword}`);
-      console.log(`${keywords.length} keywords created...`);
-      for (const product of products) {
-        await createPost(keyword, product);
-      };
+    const length = keywords.length;
+    console.log(`Keywords:: `, length);
+    // create batch
+    const batch = await createBatch(keywords);
 
-      return products;
+    for (const keyword of keywords) {
+      await createPost(keyword, batch.id);
     }
+    console.log(`${keywords.length} keywords created...`);
+    
+    return batch;
   }
 };
