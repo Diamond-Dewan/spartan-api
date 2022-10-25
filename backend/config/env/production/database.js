906b09bd8ebdc694745882fac972888ccf2ded79
Diamond <diamonddewan05@gmail.com>
2022-10-25 19:52:47 +0600

    feat: db config

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5931),
+      port: env.int('PGPORT', 5682),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'password'),
@@ -11,3 +11,23 @@ module.exports = ({ env }) => ({
     },
   },
 })
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('PGHOST', 'localhost'),
+//         port: env.int('PGPORT', 5432),
+//         database: env('PGDATABASE', 'strapi'),
+//         username: env('PGUSER', 'strapi'),
+//         password: env('PGPASSWORD', 'strapi'),
+//         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+//         ssl: env.bool(true),
+//       },
+//       options: {},
+//     },
+//   },
+// });
