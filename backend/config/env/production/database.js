f76074f29cd77a8a3c4763688ab4fd944d945763
Diamond <diamonddewan05@gmail.com>
2022-10-25 21:59:33 +0600

    fix: production database

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,33 +1,33 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5682),
-      database: env('PGDATABASE', 'railway'),
-      user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
-    },
-  },
-})
-
 // module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: env('PGHOST', 'localhost'),
-//         port: env.int('PGPORT', 5432),
-//         database: env('PGDATABASE', 'strapi'),
-//         username: env('PGUSER', 'strapi'),
-//         password: env('PGPASSWORD', 'strapi'),
-//         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
-//         ssl: env.bool(true),
-//       },
-//       options: {},
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('PGHOST', '127.0.0.1'),
+//       port: env.int('PGPORT', 5682),
+//       database: env('PGDATABASE', 'railway'),
+//       user: env('PGUSER', 'postgres'),
+//       password: env('PGPASSWORD', 'password'),
+//       ssl: env.bool(true),
 //     },
 //   },
-// });
+// })
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: false,
+      },
+      options: {},
+    },
+  },
+});
