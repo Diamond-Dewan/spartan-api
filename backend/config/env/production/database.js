27a7e480b6691777e208dba174bb6265390e9bc6
Diamond <diamonddewan05@gmail.com>
2022-10-27 03:22:11 +0600

    feat: use mysql db

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -4,16 +4,34 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('PGHOST', 'localhost'),
-        port: env.int('PGPORT', 5432),
-        database: env('PGDATABASE', 'strapi'),
-        username: env('PGUSER', 'strapi'),
-        password: env('PGPASSWORD', 'strapi'),
-        // schema: env('DATABASE_SCHEMA', 'public'), // Not Required
-        ssl: false,
+        client: 'mysql',
+        host: env('MYSQLHOST', 'localhost'),
+        port: env.int('MYSQLPORT', 3306),
+        database: env('MYSQLDATABASE', 'strapi'),
+        username: env('MYSQLUSER', 'strapi'),
+        password: env('MYSQLPASSWORD', 'strapi'),
       },
       options: {},
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('PGHOST', 'localhost'),
+//         port: env.int('PGPORT', 5432),
+//         database: env('PGDATABASE', 'strapi'),
+//         username: env('PGUSER', 'strapi'),
+//         password: env('PGPASSWORD', 'strapi'),
+//         // schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+//         ssl: false,
+//       },
+//       options: {},
+//     },
+//   },
+// });
